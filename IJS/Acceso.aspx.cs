fe9dfebbfaf87a7dbe52c8c4d93e1f2572acd7d3
Ricardo Rodriguez <ricardo.rodriguez@smartconsulting.mx>
2013-07-03 11:46:19 -0500

    CodeBehind Acceso
    Se agrega la referencia del WS IJSAALLA al proyecto, con la finalidad de
    añadir funcionalidad al formulario de Acceso, si se tiene acceso, el
    usuario puede continuar al portal de administración, en caso contrario,
    se muestra un mensaje indicandole que verifique su usuario y pass.

--- IJS/Acceso.aspx.cs
+++ IJS/Acceso.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Ext.Net;
 
 namespace IJS
 {
@@ -15,13 +16,31 @@ namespace IJS
         }
 
         /// <summary>
-        /// Recuerda validar el acceso.
+        /// Función Ligada al Botón de ACCESO.
+        /// 
+        /// Se encarga de validar el acceso al momento dar click.
+        /// 
+        /// Se llama al metodo WM_INICIA_SESSION, el cual recibe el valor de txtUser y txtPass, envía estos parametros al WS, y se encarga de verificar que
+        /// el usuario tenga acceso al portal de administración.
+        ///
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         public void ValidaAcceso(object sender, EventArgs e)
         {
-            Response.Redirect("/App/Main.aspx");
+            wsIJS.wsIJS clienteIJS = new wsIJS.wsIJS();
+            wsIJS.INICIA_SESION sesionIJS = new wsIJS.INICIA_SESION();
+
+            sesionIJS = clienteIJS.WM_INICIA_SESION(txtUser.Text, txtPass.Text);
+
+            if (sesionIJS.Error == 0)
+            {
+                Response.Redirect("/App/Main.aspx");
+            }
+            else
+            {
+                X.Msg.Alert("Error - Acceso Incorrecto", "Ha ocurrido un error al validar su inicio de sesión." + Environment.NewLine + "Verifique sus datos y vuelva a intentar").Show();
+            }
         }
     }
 }
